71613c4e9e3a2efcf498ea4ba0c3d11d7876e768
Keren Ruditsky <keren_ruditsky@hotmail.com>
2021-05-30 00:28:20 -0400

    Made function names comply with Python standards

--- VPE_script.py
+++ VPE_script.py
@@ -5,18 +5,15 @@ def main():
     if (len(sys.argv) != 3):
         print("usage: VPE_script.py <ID> \"<link>\"")
     else:
-        openFile(sys.argv[2],sys.argv[1])
+        open_file(sys.argv[2],sys.argv[1])
 
 
 
-def openFile(link, maxID):
+def open_file(link, max_id):
     count =0
-    for i in range(0,int(maxID), 50):
+    for i in range(0,int(max_id), 50):
       webbrowser.open("{}{}".format(link, i))
       count += 1
     print(str(count) + " pages have been opened")
-
-    
-
 
 main()
